8bc5d5d46941920e15a717a0dce7c97460fe6691
davpatel605-beep <davpatel605@gmail.com>
2026-03-22 05:48:00 -0700

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,19 +1,15 @@
-const axios = require("axios");
-
 export default async function handler(req, res) {
   try {
-    const API_KEY = 607add012dadcfacf6c4eb6464541fefa0d1e59068cb5240f7df1a4c350cc6a3
+    const API_KEY = "607add012dadcfacf6c4eb6464541fefa0d1e59068cb5240f7df1a4c350cc6a3";
 
-    const response = await axios.get(
+    const response = await fetch(
       `https://serpapi.com/search.json?q=iphone&engine=google_shopping&api_key=${API_KEY}`
     );
 
-    res.status(200).json(response.data);
+    const data = await response.json();
 
+    res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({
-      error: "Crash",
-      message: error.message
-    });
+    res.status(500).json({ error: error.message });
   }
 }
