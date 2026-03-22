79bb84c6f56e00bde7103f6d3822ac40ec941425
davpatel605-beep <davpatel605@gmail.com>
2026-03-22 02:54:02 -0700

    fix api crash

--- api/products.js
+++ api/products.js
@@ -1,14 +1,29 @@
 export default async function handler(req, res) {
-  export default async function handler(req, res) {
-  console.log("API HIT");
+  try {
+    const API_KEY = 607add012dadcfacf6c4eb6464541fefa0d1e59068cb5240f7df1a4c350cc6a3
 
-  const API_KEY = "607add012dadcfacf6c4eb6464541fefa0d1e59068cb5240f7df1a4c350cc6a3";
+    const response = await fetch(
+      `https://serpapi.com/search.json?q=iphone&engine=google_shopping&api_key=${API_KEY}`
+    );
 
-  const response = await fetch(
-    `https://serpapi.com/search.json?q=iphone&engine=google_shopping&api_key=${607add012dadcfacf6c4eb6464541fefa0d1e59068cb5240f7df1a4c350cc6a3}`
-  );
+    const data = await response.json();
 
-  const data = await response.json();
+    const products = data.shopping_results || [];
 
-  res.status(200).json(data);
+    const validProducts = products.filter(p => p.price);
+
+    const best = validProducts.reduce((lowest, current) => {
+      const price1 = parseInt(lowest.price.replace(/[^0-9]/g, ""));
+      const price2 = parseInt(current.price.replace(/[^0-9]/g, ""));
+      return price2 < price1 ? current : lowest;
+    }, validProducts[0]);
+
+    res.status(200).json({
+      bestProduct: best,
+      allProducts: validProducts
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: "Something went wrong", details: error.message });
+  }
 }
