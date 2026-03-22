7140eae3a2acd97eaf7fce2e84d7adfe53067d98
davpatel605-beep <davpatel605@gmail.com>
2026-03-22 05:30:54 -0700

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+const axios = require("axios");
 
 export default async function handler(req, res) {
   try {
-    const API_KEY =607add012dadcfacf6c4eb6464541fefa0d1e59068cb5240f7df1a4c350cc6a3
-    
+    const API_KEY = 607add012dadcfacf6c4eb6464541fefa0d1e59068cb5240f7df1a4c350cc6a3
+
     const response = await axios.get(
       `https://serpapi.com/search.json?q=iphone&engine=google_shopping&api_key=${API_KEY}`
     );
