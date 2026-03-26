15f91ad90cd62efc71de1d1cdd85fdbcc2bae1e3
davpatel605-beep <davpatel605@gmail.com>
2026-03-25 21:11:01 -0700

    Delete src/lib/hello.ts

--- src/lib/hello.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://wolhksrjrossztdsuuly.supabase.co";
-const supabaseKey = "sb_publishable_UltwN-C9DGrlNgiFzu1Auw_V5MY-Mos";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
